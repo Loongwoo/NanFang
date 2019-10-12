4f1a7d5f76033f1acd4c7940a7ba0cad3e708459
Loongwoo <xulongwu@kiwik.cn>
2019-10-12 09:45:57 +0800

    fix

--- src/renderer/components/CustomIcon.tsx
+++
@@ -1,8 +0,0 @@
-import { Icon } from 'antd';
-import { IconProps } from 'antd/lib/icon';
-
-const scriptUrl = 'http://at.alicdn.com/t/font_1164844_40hoeza035u.js';
-
-export const CustomIcon: React.SFC<IconProps> = Icon.createFromIconfontCN({
-  scriptUrl,
-});
